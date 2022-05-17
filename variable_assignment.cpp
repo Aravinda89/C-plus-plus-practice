ef526a4a04ca83cb51adb7e1d2626d88ff701f53
Aravinda89 <1513223865@qq.com>
2022-05-17 21:11:47 +0800

    Create variable_assignment.cpp
    variable assignment

---
+++ variable_assignment.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+   int numerator = 4;
+   float denominator = 5;
+   float answer = 0;
+
+   answer = numerator / denominator;
+   std::cout<<"answer = "<<answer;
+   return 0;
+}
